8ed5059d42dab4e471f3c5e908fa9810b97c69ed
Kosta <68782786+KostaD02@users.noreply.github.com>
2022-03-12 22:09:41 +0400

    added fonts and angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomepageComponent } from './views/homepage/homepage.component';
+import { NotfoundComponent } from './views/notfound/notfound.component';
+import { MaterialModule } from './module/material/material.module';
+import { HeaderComponent } from './shared/header/header.component';
+import { LoaderComponent } from './shared/loader/loader.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HomepageComponent, NotfoundComponent, HeaderComponent, LoaderComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
